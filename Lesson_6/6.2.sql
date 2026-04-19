a168494aa132fbfeedeb187b7926a163b768d1d2
hiroto <6hiroto9@gmail.com>
2026-04-19 21:23:36 +0900

    第6章 練習問題

---
+++ Lesson_6/6.2.sql
@@ -0,0 +1,20 @@
+-- 価格帯分類SELECT文
+SELECT
+    SUM( 
+        CASE 
+            WHEN hanbai_tanka <= 1000 
+                THEN 1 
+            ELSE 0 
+            END
+    ) AS low_price
+    , SUM( 
+        CASE 
+            WHEN hanbai_tanka > 1000 
+            AND hanbai_tanka <= 3000 
+                THEN 1 
+            ELSE 0 
+            END
+    ) AS mid_price
+    , SUM(CASE WHEN hanbai_tanka > 3000 THEN 1 ELSE 0 END) AS high_price 
+FROM
+    Shohin;
